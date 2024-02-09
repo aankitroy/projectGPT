4989d03dbdb01da31e19f526676f3df6775f4ac5
Aankit Roy <aankit@getkhabri.com>
2024-02-09 13:16:11 +0530

    get assistant name change

--- app/helperAI.py
+++ app/helperAI.py
@@ -9,7 +9,8 @@ client = OpenAI(settings.OPEN_AI_API_KEY)
 def connect_to_openAI():
     client = OpenAI(settings.OPEN_AI_API_KEY)
     
-def get_assistant(assistant_id: str):
+def get_assistant_by_id(assistant_id: str):
+    global client
     if client is None:
         connect_to_openAI()
     assistant = client.beta.assistants.retrieve(assistant_id)
